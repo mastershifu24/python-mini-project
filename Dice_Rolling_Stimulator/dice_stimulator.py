f8fdc50e587d1efb175b7d964f3b8953d7a0bcfa
Ahmed Shifa <ahmedshifa298@gmail.com>
2023-11-20 09:37:25 -0500

    Update dice_stimulator.py
    updated code, the dice faces are stored as a list of strings in the DICE_FACES constant. The roll_dice() function selects the appropriate face based on the randomly generated number and prints it. The code will continue to roll the dice until the user enters something other than 'y'.
    
    These improvements can enhance code readability, maintainability, and reusability.

--- Dice_Rolling_Stimulator/dice_stimulator.py
+++ Dice_Rolling_Stimulator/dice_stimulator.py
@@ -1,50 +1,60 @@
 import random
 
-print("This is a dice stimulator")
-x = "y"
-while x == "y":
-    number = random.randint(1,6)
+DICE_FACES = [
+    [
+        "===========",
+        "|         |",
+        "|    O    |",
+        "|         |",
+        "==========="
+    ],
+    [
+        "===========",
+        "|         |",
+        "| O     O |",
+        "|         |",
+        "==========="
+    ],
+    [
+        "===========",
+        "|    O    |",
+        "|    O    |",
+        "|    O    |",
+        "==========="
+    ],
+    [
+        "===========",
+        "| O     O |",
+        "|         |",
+        "| O     O |",
+        "==========="
+    ],
+    [
+        "===========",
+        "| O     O |",
+        "|    O    |",
+        "| O     O |",
+        "==========="
+    ],
+    [
+        "===========",
+        "| O     O |",
+        "| O     O |",
+        "| O     O |",
+        "==========="
+    ]
+]
 
-    if number == 1:
-        print("===========")
-        print("|         |")
-        print("|    O    |")
-        print("|         |")
-        print("===========")
+def roll_dice():
+    number = random.randint(1, 6)
+    dice_face = DICE_FACES[number - 1]
 
-    if number == 2:
-        print("===========")
-        print("|         |")
-        print("| O     O |")
-        print("|         |")
-        print("===========")
+    for line in dice_face:
+        print(line)
 
-    if number == 3:
-        print("===========")
-        print("|    O    |")
-        print("|    O    |")
-        print("|    O    |")
-        print("===========")
-        
-    if number == 4:
-        print("===========")
-        print("| O     O |")
-        print("|         |")
-        print("| O     O |")
-        print("===========")
-        
-    if number == 5:
-        print("===========")
-        print("| O     O |")
-        print("|    O    |")
-        print("| O     O |")
-        print("===========")
-        
-    if number == 6:
-        print("===========")
-        print("| O     O |")
-        print("| O     O |")
-        print("| O     O |")
-        print("===========")
-    
-    x =  input("Press y to roll again ")
+print("This is a dice simulator")
+while True:
+    roll_dice()
+    x = input("Press y to roll again (or any other key to exit): ")
+    if x != "y":
+        break
